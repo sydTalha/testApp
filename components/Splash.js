6074eb20d44a5867972ab1f4fe9490ad5d66029e
Talha <talharizwan013@gmail.com>
2018-10-15 18:14:12 +0500

    first commit

---
+++ components/Splash.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, View, StatusBar, Image} from 'react-native';
+
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+            barStyle="light-content"
+            backgroundColor="#000"/>
+        <Image 
+            source={require('../images/splash.png')}
+            style={{width:100, height:100}}
+        /> 
+      </View>
+    )
+  }
+}
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+})
